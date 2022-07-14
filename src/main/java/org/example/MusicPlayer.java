7ff739c7677d13b0529006f8e18c46500f1c0b5c
Leonid Barsucovschi <leonidbars93leon@gmail.com>
2022-07-14 17:04:54 +0300

    add musicPlayer.properties and other code

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,10 +3,35 @@ package org.example;
 public class MusicPlayer {
 
     private Music music;
+    private String name;
+    private int volume;
 
+
+    public MusicPlayer() {
+    }
 
     // IoC - Inversion of Control
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
